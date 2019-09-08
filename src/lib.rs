4b6812ed9719825af61e4e905928bab3f9a27996
Ben Morrison <ben@gbmor.dev>
2019-09-08 14:30:07 -0400

    client and pull twtxt.txt

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,37 @@
+#![allow(dead_code)]
+
+use std::time::Duration;
+
+use reqwest;
+
+pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+fn build_client() -> Result<reqwest::Client> {
+    let client = reqwest::Client::builder()
+        .gzip(true)
+        .timeout(Duration::from_secs(10))
+        .build()?;
+    Ok(client)
+}
+
+pub fn pull_twtxt(url: &str) -> Result<String> {
+    let client = build_client()?;
+    let res = client.get(url).send()?.text()?;
+    Ok(res)
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
-    fn init() {
-        assert_eq!(2 + 2, 4);
+    fn test_build_client() {
+        // Right now, just panic if it returns Err()
+        build_client().unwrap();
+    }
+    #[test]
+    fn test_pull_twtxt() {
+        let res = pull_twtxt("https://gbmor.dev/twtxt.txt").unwrap();
+        eprintln!("{}", res);
+        assert!(res.contains("gbmor"));
     }
 }
