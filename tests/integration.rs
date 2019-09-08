d459a3a357897a0a17b8a4d0ad78878afe2b1200
Ben Morrison <ben@gbmor.dev>
2019-09-08 19:57:21 -0400

    more testing

---
+++ tests/integration.rs
@@ -0,0 +1,12 @@
+use rustwtxt;
+use rustwtxt::parse;
+
+#[test]
+fn end_to_end() {
+    let twtxt = rustwtxt::pull_twtxt("https://gbmor.dev/twtxt.txt").unwrap();
+    let user = parse::metadata(&twtxt, "nick", 1);
+    assert_eq!(&user, "gbmor");
+
+    let statuses = parse::statuses(&twtxt);
+    assert!(statuses.len() > 1);
+}
