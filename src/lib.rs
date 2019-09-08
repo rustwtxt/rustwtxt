ba16da0d7a69fcd41a26394122e3e527bbbace57
Ben Morrison <ben@gbmor.dev>
2019-09-08 19:07:32 -0400

    pulled out some funcs/tests to module parse

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
 #![allow(dead_code)]
 
-use std::collections::BTreeMap;
 use std::time::Duration;
 
-use chrono::prelude::*;
 use reqwest;
 
-pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+pub mod parse;
+
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
 fn build_client() -> Result<reqwest::Client> {
     let client = reqwest::Client::builder()
@@ -33,62 +33,6 @@ pub fn pull_twtxt(url: &str) -> Result<String> {
     Ok(res)
 }
 
-/// This parses out the information in the "== Metadata ==" section of
-/// a given `twtxt.txt` file.
-/// You'll have to play with the trim offset here. Often there will be
-/// unprintable characters, such as newlines, appended despite the output
-/// being `trim()`-ed before return.
-///
-/// # Examples
-/// ```
-/// # use rustwtxt::*;
-/// let twtxt = pull_twtxt("https://example.org/twtxt.txt").unwrap();
-/// let out = parse_metadata(&twtxt, "nick", 1);
-/// ```
-pub fn parse_metadata(twtxt: &str, keyword: &str, trim: usize) -> String {
-    if !twtxt.contains("== Metadata ==") || !twtxt.contains(keyword) {
-        return String::new();
-    }
-
-    let split = twtxt.split(keyword).collect::<Vec<&str>>();
-    let split = split[1];
-    let split = split.split("=").collect::<Vec<&str>>();
-    let split = split[1];
-    let split = split.split(" ").collect::<Vec<&str>>();
-    let word = split[1];
-    let word = word[..word.len() - trim].to_string();
-    word.trim().into()
-}
-
-/// Pull the individual statuses from a remote `twtxt.txt` file into
-/// a `std::collections::BTreeMap`, with the dates parsed into a
-/// `chrono::DateTime<FixedOffset>`
-///
-/// # Examples
-/// ```
-///# use rustwtxt::*;
-/// let twtxt = pull_twtxt("https://example.org/twtxt.txt").unwrap();
-/// let status_map = get_statuses(&twtxt);
-/// ```
-pub fn get_statuses(twtxt: &str) -> BTreeMap<chrono::DateTime<FixedOffset>, String> {
-    let mut map = BTreeMap::<chrono::DateTime<FixedOffset>, String>::new();
-    let lines = twtxt.split("\n").collect::<Vec<&str>>();
-    lines.iter().for_each(|line| {
-        if line.starts_with("#") || line.len() < 1 {
-            return;
-        }
-
-        let status = line.split("\t").collect::<Vec<&str>>();
-        let datestamp = if let Ok(val) = DateTime::parse_from_rfc3339(status[0]) {
-            val
-        } else {
-            return;
-        };
-        map.insert(datestamp, status[1].into());
-    });
-    map
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -108,44 +52,8 @@ mod tests {
     }
 
     #[test]
-    fn test_get_username() {
-        let res = pull_twtxt(TEST_URL).unwrap();
-        let user = parse_metadata(&res, "nick", 1);
-        assert_eq!("gbmor", &user);
-    }
-
-    // This passes `cargo test`, but `cargo tarpaulin` segfaults
-    #[ignore]
-    #[test]
-    fn test_get_url() {
-        let res = pull_twtxt(TEST_URL).unwrap();
-        let url = parse_metadata(&res, "url", 4);
-        assert_eq!(TEST_URL, &url);
-    }
-
-    #[test]
-    fn test_status_map() {
-        let twtxt = pull_twtxt(TEST_URL).unwrap();
-        let res = get_statuses(&twtxt);
-        eprintln!("{:#?}", res);
-        assert!(res.len() > 1);
-    }
-
-    #[test]
     #[should_panic]
     fn test_bad_url() {
         pull_twtxt("https://example-some-fake-site-goes-here.com/some_fake_url.txt").unwrap();
-    }
-
-    #[test]
-    fn parse_bad_twtxt() {
-        let rhs = parse_metadata("SOMETHING GOES HERE", "url", 0);
-        assert_eq!(String::new(), rhs);
-    }
-
-    #[test]
-    fn get_bad_statuses() {
-        let out = get_statuses("2019-04-50\tSOME STATUS");
-        assert!(out.len() < 1);
     }
 }
---
+++ src/parse.rs
@@ -0,0 +1,103 @@
+use std::collections::BTreeMap;
+
+use chrono::prelude::*;
+
+/// This parses out the information in the "== Metadata ==" section of
+/// a given `twtxt.txt` file.
+/// You'll have to play with the trim offset here. Often there will be
+/// unprintable characters, such as newlines, appended despite the output
+/// being `trim()`-ed before return.
+///
+/// # Examples
+/// ```
+/// # use rustwtxt::*;
+/// # use rustwtxt::parse;
+/// let twtxt = pull_twtxt("https://example.org/twtxt.txt").unwrap();
+/// let out = parse::metadata(&twtxt, "nick", 1);
+/// ```
+pub fn metadata(twtxt: &str, keyword: &str, trim: usize) -> String {
+    if !twtxt.contains("== Metadata ==") || !twtxt.contains(keyword) {
+        return String::new();
+    }
+
+    let split = twtxt.split(keyword).collect::<Vec<&str>>();
+    let split = split[1];
+    let split = split.split("=").collect::<Vec<&str>>();
+    let split = split[1];
+    let split = split.split(" ").collect::<Vec<&str>>();
+    let word = split[1];
+    let word = word[..word.len() - trim].to_string();
+    word.trim().into()
+}
+
+/// Pull the individual statuses from a remote `twtxt.txt` file into
+/// a `std::collections::BTreeMap`, with the dates parsed into a
+/// `chrono::DateTime<FixedOffset>`
+///
+/// # Examples
+/// ```
+///# use rustwtxt::*;
+///# use rustwtxt::parse;
+/// let twtxt = pull_twtxt("https://example.org/twtxt.txt").unwrap();
+/// let status_map = parse::statuses(&twtxt);
+/// ```
+pub fn statuses(twtxt: &str) -> BTreeMap<chrono::DateTime<FixedOffset>, String> {
+    let mut map = BTreeMap::<chrono::DateTime<FixedOffset>, String>::new();
+    let lines = twtxt.split("\n").collect::<Vec<&str>>();
+    lines.iter().for_each(|line| {
+        if line.starts_with("#") || line.len() < 1 {
+            return;
+        }
+
+        let status = line.split("\t").collect::<Vec<&str>>();
+        let datestamp = if let Ok(val) = DateTime::parse_from_rfc3339(status[0]) {
+            val
+        } else {
+            return;
+        };
+        map.insert(datestamp, status[1].into());
+    });
+    map
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TEST_URL: &str = "https://gbmor.dev/twtxt.txt";
+
+    #[test]
+    fn test_get_username() {
+        let res = crate::pull_twtxt(TEST_URL).unwrap();
+        let user = metadata(&res, "nick", 1);
+        assert_eq!("gbmor", &user);
+    }
+
+    // This passes `cargo test`, but `cargo tarpaulin` segfaults
+    #[ignore]
+    #[test]
+    fn test_get_url() {
+        let res = crate::pull_twtxt(TEST_URL).unwrap();
+        let url = metadata(&res, "url", 4);
+        assert_eq!(TEST_URL, &url);
+    }
+
+    #[test]
+    fn test_status_map() {
+        let twtxt = crate::pull_twtxt(TEST_URL).unwrap();
+        let res = statuses(&twtxt);
+        eprintln!("{:#?}", res);
+        assert!(res.len() > 1);
+    }
+    #[test]
+    fn parse_bad_twtxt() {
+        let rhs = metadata("SOMETHING GOES HERE", "url", 0);
+        assert_eq!(String::new(), rhs);
+    }
+
+    #[test]
+    fn get_bad_statuses() {
+        let out = statuses("2019-04-50\tSOME STATUS");
+        assert!(out.len() < 1);
+    }
+}
